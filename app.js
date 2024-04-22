68328d1d3fee045f80e26191bf271295966801c1
evanseamons <evan.seamons1572@stu.mtec.edu>
2024-04-22 16:27:31 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$('li').css('background-color', '#ccc');
+$('li:odd').css('background-color', '#aaa');
+$('input').val('Evan');
